6e6039ec5f2e8027d09cd9ca1b022c1b49e9b50b
Raymond Gasper <14928724+rgasper@users.noreply.github.com>
2022-12-30 08:39:33 -0500

    risetnrseit

--- astrodesktop/pics/forms.py
+++ astrodesktop/pics/forms.py
@@ -1,12 +1,9 @@
 from django import forms
 from pics.models import Picture
 
+# NOTE unused
+
 
 class PictureForm(forms.ModelForm):
     class Meta:
         model = Picture
-
-
-# PictureFormSet = forms.modelformset_factory(
-# Picture
-# )
